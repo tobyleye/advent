81341da5e5c02958b37dac875020faf5e191893a
tobi <krisella74@gmail.com>
2021-04-05 08:42:36 +0100

    answer to question 4b

---
+++ 2020/day03.py
@@ -0,0 +1,24 @@
+lines = [line.strip() for line in open('day03.txt')]
+_lines = ''.join(lines)
+line_length = len(lines[0])
+
+col = 0
+for line in lines[1:]:
+    next_col = col + 3
+    if next_col > len(line):
+        next_col = 
+'''
+trees_count = 0
+tree = '#'
+for n, line in enumerate(lines[1:], start=1):
+    checkin_idx = n * 3
+    try: 
+        checkin = line[checkin_idx]
+        if checkin == tree:
+            trees_count += 1
+    except IndexError: 
+        break
+'''
+
+
+print(trees_count)
---
+++ 2020/day04.py
@@ -0,0 +1,64 @@
+import re
+
+f = open('day04.txt').read()
+required_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', ]
+
+valid_passports = 0
+valid_passports_part2 = 0
+
+passports = f.split('\n\n')
+
+def within_range(num, start, end):
+    try:
+        num = int(num)
+        return start <= num <= end
+    except:
+        return False
+
+def validate_pattern(value, pattern):
+    if re.search(pattern, value):
+        return True
+    return False
+
+def validate_hgt(string):
+    search = re.search('^(\d+)(cm|in)$', string)
+    if search:
+        num, unit = search.groups();
+        return (unit == 'cm' and within_range(num, 150, 193)) or \
+                (unit == 'in' and within_range(num, 59, 76))
+
+schema = {
+    'byr': (within_range, 1920, 2002),
+    'iyr': (within_range, 2010, 2020),
+    'eyr': (within_range, 2020, 2030),
+    'hgt': (validate_hgt,),
+    'hcl': (validate_pattern, r'^#[0-9a-f]{6}$'),
+    'ecl': (lambda value: value in ('amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'),),
+    'pid': (lambda value: len(value) == 9, )
+}
+
+for p in passports:
+    passport = {}
+    for key_value in p.split():
+        key, value = key_value.split(':')
+        passport[key] = value
+    
+    # part #1
+    if all(field in passport for field in required_fields):
+        valid_passports += 1
+
+        # part #2
+        isValid = True
+        for (key, validator) in schema.items():
+            func, *args = validator
+            value = passport[key]
+            isValid = isValid and func(value, *args)
+
+        if isValid:
+            valid_passports_part2+= 1
+
+# part #2
+print('valid passports:', valid_passports)
+print('valid passports part #2:', valid_passports_part2)
+
+

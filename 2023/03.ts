0aae25cc04fca14d6a0747ab6187e714774212a1
Oluwatobi <krisella74@gmail.com>
2023-12-09 23:05:54 +0100

    day 3, part 1

--- 2023/03.ts
+++ 2023/03.ts
@@ -1,3 +1,68 @@
 import fs from "fs";
 
-let input = fs.readFileSync("./03.txt");
+let input = fs.readFileSync("./03.txt", "utf8");
+let inputLines = input.split("\n");
+
+const isAdjacentToSymbol = (
+  numberStartIndex: number,
+  numberEndIndex: number,
+  currentLine: String,
+  prevLine: string | undefined,
+  nextLine: string | undefined
+) => {
+  let lines = [currentLine, prevLine, nextLine];
+  for (let line of lines) {
+    if (line) {
+      let startSearch = Math.max(0, numberStartIndex - 1);
+      let endSearch = numberEndIndex + 2;
+      let numberSearchArea = line.slice(startSearch, endSearch);
+      if (numberSearchArea.search(/[^\d\.]/) > -1) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+let total = 0;
+
+for (let lineIndex = 0; lineIndex < inputLines.length; lineIndex++) {
+  let line = inputLines[lineIndex];
+  let startSearchIndex = 0;
+  while (startSearchIndex < line.length) {
+    let numStartIndex = line.slice(startSearchIndex).search(/\d+/);
+    if (numStartIndex > -1) {
+      // number found
+      numStartIndex = numStartIndex + startSearchIndex;
+      let num = line[numStartIndex];
+      let numEndIndex;
+      let i = numStartIndex + 1;
+      for (; i < line.length; i++) {
+        if (line[i].search(/\d+/) > -1) {
+          num = num.concat(line[i]);
+        } else {
+          break;
+        }
+      }
+      numEndIndex = i - 1;
+
+      let previousLine = inputLines[lineIndex - 1];
+      let nextLine = inputLines[lineIndex + 1];
+      if (
+        isAdjacentToSymbol(
+          numStartIndex,
+          numEndIndex,
+          line,
+          previousLine,
+          nextLine
+        )
+      ) {
+        total += Number(num);
+      }
+      startSearchIndex = i;
+    } else {
+      startSearchIndex++;
+    }
+  }
+}
+console.log({ total });
